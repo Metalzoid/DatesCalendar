9481c537bf6bfcd02715c097ea9fea23003d82a2
Florian Gagnaire <gagnaire.flo@gmail.com>
2024-07-30 10:41:29 +0200

    edit redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,11 +1,11 @@
 url = ENV["REDIS_URL"]
 
 if url
-  Sidekiq.configure_client do |config|
-    config.redis = { url: ENV['REDIS_URL'], size: 1}
+  Sidekiq.configure_server do |config|
+    config.redis = { url: url }
   end
 
-  Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDIS_URL']}
+  Sidekiq.configure_client do |config|
+    config.redis = { url: url }
   end
 end
