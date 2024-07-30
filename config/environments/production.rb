363eb77ddfa3daefeb207b5643dd6e5913bcb952
Florian Gagnaire <gagnaire.flo@gmail.com>
2024-07-30 09:34:11 +0200

    editing DB to PGSQL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in ENV["RAILS_MASTER_KEY"], config/master.key, or an environment
   # key such as config/credentials/production.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = true
 
   # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
   # config.public_file_server.enabled = false
@@ -106,4 +106,6 @@ Rails.application.configure do
   # Required for all session management (regardless of session_store)
   config.middleware.use ActionDispatch::Cookies
   config.middleware.use config.session_store, config.session_options
+
+  config.active_record.sqlite3_production_warning=false
 end
