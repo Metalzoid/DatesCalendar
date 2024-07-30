be4207e74601d85415e67cd0e1d2d50cdd9ea9e9
Florian Gagnaire <gagnaire.flo@gmail.com>
2024-07-30 09:23:06 +0200

    Modifying origins allowed for cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'http://31.36.8.62'
     resource '*',
       headers: :any,
       expose: ['Authorization'],
