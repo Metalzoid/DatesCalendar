2cbc6ef5284f0f51783e4ae43ce69e2b49aaf3cb
Florian Gagnaire <gagnaire.flo@gmail.com>
2024-08-25 23:48:45 +0200

    Edit oas_rails conf + delete Procfile + edit docker-entrypoint

--- config/initializers/oas_rails.rb
+++ config/initializers/oas_rails.rb
@@ -1,70 +1,68 @@
 # config/initializers/oas_rails.rb
-unless Rails.env.production?
-  OasRails.configure do |config|
-    # Basic Information about the API
-    config.info.title = 'DatesCalendar'
-    config.info.summary = 'A simple Appointment API.'
-    config.info.description = "[Go to Admininistration interface](/admin)"
-    config.info.contact.name = 'Florian GAGNAIRE'
-    config.info.contact.email = 'gagnaire.flo@gmail.com'
-    config.info.contact.url = 'http://florian-gagnaire.dev'
+OasRails.configure do |config|
+  # Basic Information about the API
+  config.info.title = 'DatesCalendar'
+  config.info.summary = 'A simple Appointment API.'
+  config.info.description = "[Go to Admininistration interface](/admin)"
+  config.info.contact.name = 'Florian GAGNAIRE'
+  config.info.contact.email = 'gagnaire.flo@gmail.com'
+  config.info.contact.url = 'http://florian-gagnaire.dev'
 
-    # Servers Information. For more details follow: https://spec.openapis.org/oas/latest.html#server-object
-    config.servers = [{ url: 'http://localhost:3001', description: 'Development' }, { url: 'http://datescalendar.fr', description: 'Production'}]
+  # Servers Information. For more details follow: https://spec.openapis.org/oas/latest.html#server-object
+  config.servers = [{ url: 'http://localhost:3001', description: 'Development' }, { url: 'http://datescalendar.fr', description: 'Production'}]
 
-    # Tag Information. For more details follow: https://spec.openapis.org/oas/latest.html#tag-object
-    # config.tags = [{ name: "services", description: "Manage the `amazing` Services." }]
+  # Tag Information. For more details follow: https://spec.openapis.org/oas/latest.html#tag-object
+  # config.tags = [{ name: "services", description: "Manage the `amazing` Services." }]
 
-    # Optional Settings (Uncomment to use)
+  # Optional Settings (Uncomment to use)
 
-    # Extract default tags of operations from namespace or controller. Can be set to :namespace or :controller
-    config.default_tags_from = :controller
+  # Extract default tags of operations from namespace or controller. Can be set to :namespace or :controller
+  config.default_tags_from = :controller
 
-    # Automatically detect request bodies for create/update methods
-    # Default: true
-    # config.autodiscover_request_body = false
+  # Automatically detect request bodies for create/update methods
+  # Default: true
+  # config.autodiscover_request_body = false
 
-    # Automatically detect responses from controller renders
-    # Default: true
-    config.autodiscover_responses = false
+  # Automatically detect responses from controller renders
+  # Default: true
+  config.autodiscover_responses = false
 
-    # API path configuration if your API is under a different namespace
-    config.api_path = "/api/v1/"
+  # API path configuration if your API is under a different namespace
+  config.api_path = "/api/v1/"
 
-    # #######################
-    # Authentication Settings
-    # #######################
+  # #######################
+  # Authentication Settings
+  # #######################
 
-    # Whether to authenticate all routes by default
-    # Default is true; set to false if you don't want all routes to include secutrity schemas by default
-    config.authenticate_all_routes_by_default = false
+  # Whether to authenticate all routes by default
+  # Default is true; set to false if you don't want all routes to include secutrity schemas by default
+  config.authenticate_all_routes_by_default = false
 
-    # Default security schema used for authentication
-    # Choose a predefined security schema
-    # [:api_key_cookie, :api_key_header, :api_key_query, :basic, :bearer, :bearer_jwt, :mutual_tls]
-    config.security_schema = :bearer_jwt
+  # Default security schema used for authentication
+  # Choose a predefined security schema
+  # [:api_key_cookie, :api_key_header, :api_key_query, :basic, :bearer, :bearer_jwt, :mutual_tls]
+  config.security_schema = :bearer_jwt
 
-    # Custom security schemas
-    # You can uncomment and modify to use custom security schemas
-    # Please follow the documentation: https://spec.openapis.org/oas/latest.html#security-scheme-object
-    #
-    # config.security_schemas = {
-    #  bearer:{
-    #   "type": "apiKey",
-    #   "name": "api_key",
-    #   "in": "header"
-    #  }
-    # }
+  # Custom security schemas
+  # You can uncomment and modify to use custom security schemas
+  # Please follow the documentation: https://spec.openapis.org/oas/latest.html#security-scheme-object
+  #
+  # config.security_schemas = {
+  #  bearer:{
+  #   "type": "apiKey",
+  #   "name": "api_key",
+  #   "in": "header"
+  #  }
+  # }
 
-    # ###########################
-    # Default Responses (Errors)
-    # ###########################
+  # ###########################
+  # Default Responses (Errors)
+  # ###########################
 
-    # The default responses errors are setted only if the action allow it.
-    # Example, if you add forbidden then it will be added only if the endpoint requires authentication.
-    # Example: not_found will be setted to the endpoint only if the operation is a show/update/destroy action.
-    config.set_default_responses = false
-    # config.possible_default_responses = [:not_found, :unauthorized, :forbidden]
-    # config.response_body_of_default = { message: String }
-  end
+  # The default responses errors are setted only if the action allow it.
+  # Example, if you add forbidden then it will be added only if the endpoint requires authentication.
+  # Example: not_found will be setted to the endpoint only if the operation is a show/update/destroy action.
+  config.set_default_responses = false
+  # config.possible_default_responses = [:not_found, :unauthorized, :forbidden]
+  # config.response_body_of_default = { message: String }
 end
