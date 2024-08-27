e058664116bda9171e4b1b774f4b34dbed17e5c7
Florian GAGNAIRE <gagnaire.flo@gmail.com>
2024-08-27 15:37:57 +0200

    OAS_RAILS Docs done for the moment

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-
+require "pry-byebug"
 module Users
   class RegistrationsController < Devise::RegistrationsController
     # @summary Register new User
@@ -8,10 +8,12 @@ module Users
     # - Password minimum length: 6 chars.
     # - Optionnal: company.
     # @parameter APIKEY(header) [String!] The admin APIKEY.
-    # @request_body The user informations. At least include an `email`. [Hash!] {user: {email: String, password: String, firstname: String, lastname: String, company: String, role: String}}
+    # @request_body The user informations. At least include an `email`. [!Hash{user: Hash{email: String, password: String, firstname: String, lastname: String, company: String, role: String}}]
     # @request_body_example basic user [Hash] {user: {email: "test@gmail.com", password: "azerty", firstname: "Pedro", lastname: "Pedro", role: "seller"}}
-    # @response Logged in Successfully.(200) [Hash] {message: String, data: Hash}
-    # @response User couldn't be created successfully. Admin must exist and Admin can't be blank.(422) [Hash] {message: String}
+    # @response Logged in Successfully.(200) [Hash{message: String, data: Hash{user: Hash{id: Integer, email: String, firstname: String, lastname: String, company: String, role: String}}}]
+    # @response_example Logged in Successfully.(200) [{message: "Logged in Successfully.", data: {user: {id: 2, email: "test@gmail.com", firstname: "Pedro", lastname: "Pedro", company: "", role: "seller"}}}]
+    # @response User couldn't be created successfully. Admin must exist and Admin can't be blank.(422) [Hash{message: String}]
+    # @response_example User couldn't be created successfully. Admin must exist and Admin can't be blank.(422) [{message: "User couldn't be created successfully. Admin must exist and Admin can't be blank."}]
     # @tags Users
     def create
       @user = build_resource(sign_up_params)
@@ -43,10 +45,40 @@ module Users
     #   super
     # end
 
-    # PUT /resource
-    # def update
-    #   super
-    # end
+    # @summary Edit a User
+    # - Required: current_password
+    # @request_body The user informations. At least include an `email`. [!Hash{user: Hash{email: String, current_password: String, password: String, password_confirmation: String, firstname: String, lastname: String, company: String, role: String}}]
+    # @request_body_example basic user [Hash] {user: {email: "test2@gmail.com", current_password: "azerty", role: "both"}}
+    # @response User has been updated.(200) [Hash{message: String, data: Hash{user: Hash{id: Integer, email: String, firstname: String, lastname: String, company: String, role: String}}}]
+    # @response_example User has been updated.(200) [{message: "User has been updated.", data: {user: {id: 2, email: "test2@gmail.com", firstname: "Pedro", lastname: "Pedro", company: "", role: "both"}}}]
+    # @response Bearer JWT Token required !(401) [Hash{message: String}]
+    # @response_example Bearer JWT Token required !(401) [{message: "Bearer JWT Token required !"}]
+    # @response User has not been updated.(422) [Hash{message: String}]
+    # @response_example User has not been updated.(422) [{message: "User has not been updated. => error message"}]
+    # @tags Users
+    # @auth [bearer_jwt]
+    def update
+      return render json: {message: "Bearer JWT Token required !"}, status: :unauthorized unless request.headers['Authorization'].present?
+      if request.headers['Authorization'].present?
+        jwt_payload = JWT.decode(request.headers['Authorization'].split(' ').last, ENV['DEVISE_JWT_SECRET_KEY']).first
+        current_api_user = User.find(jwt_payload['sub'])
+      end
+      self.resource = current_api_user
+      prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+
+      resource_updated = update_resource(resource, account_update_params)
+      yield resource if block_given?
+      if resource_updated
+        set_flash_message_for_update(resource, prev_unconfirmed_email)
+        bypass_sign_in resource, scope: resource_name if sign_in_after_change_password?
+
+        render json: {message: "User has been updated.", data: {user: UserSerializer.new(self.resource).serializable_hash[:data][:attributes]}}, status: :ok
+      else
+        clean_up_passwords resource
+        set_minimum_password_length
+        render json: {message: "User has not been updated. #{self.resource.errors.messages}"}, status: :unprocessable_entity
+      end
+    end
 
     # DELETE /resource
     # def destroy
@@ -70,9 +102,9 @@ module Users
     # end
 
     # If you have extra params to permit, append them to the sanitizer.
-    # def configure_account_update_params
-    #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-    # end
+    def configure_account_update_params
+      devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+    end
 
     # The path used after sign up.
     # def after_sign_up_path_for(resource)
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -7,6 +7,10 @@ module Users
 
 
     # @summary Get current user informations
+    # @response Data of the current user connected.(200) [Hash{message: String, data: Hash{id: Integer, email: String, firstname: String, lastname: String, company: String, role: String }}]
+    # @response_example Data of the current user connected.(200) [{message: "Data of the current user connected.", data: {id: 2, email: "test@gmail.com", firstname: "Pedro", lastname: "Pedro", company: "", role: "seller"}}]
+    # @response Couldn't find an active session.(401) [Hash{message: String}]
+    # @response_example Couldn't find an active session.(401) [{message: "Couldn't find an active session."}]
     # @tags Users
     # @auth [bearer_jwt]
     def new
@@ -22,10 +26,12 @@ module Users
 
     # @summary Login as User
     # - Return the JWT token in the headers.
-    # @request_body The user login. At least include an `email`. [!Hash{user: {email: String, password: String}}]
-    # @request_body_example basic user [Hash{user: {email: "test@gmail.com", password: "azerty"}}]
+    # @request_body The user login. At least include an `email`. [!Hash{user: Hash{email: String, password: String}}]
+    # @request_body_example basic user [Hash] {user: {email: "test@gmail.com", password: "azerty"}}
     # @response Invalid Email or password.(401) [Hash{error: String}]
-    # @response Logged in Successfully.(200) [Hash{message: String, data: Hash}]
+    # @response_example Invalid Email or password.(401) [{error: "Invalid Email or password."}]
+    # @response Logged in Successfully.(200) [Hash{message: String, data: Hash{user: Hash{id: Integer, email: String, firstname: String, lastname: String, company: String, role: String}}}]
+    # @response_example Logged in Successfully.(200) [{message: "Logged in Successfully.", data: {user: {id: 2, email: "test@gmail.com", firstname: "Pedro", lastname: "Pedro", company: "", role: "seller"}}}]
     # @tags Users
     # @no_auth
     def create
@@ -33,11 +39,12 @@ module Users
     end
 
     # @summary Logout as User
-    # @parameter Authorization(header) [String!] The Authorization JWT token.
     # @response Couldn't find an active session.(401) [Hash{message: String}]
+    # @response_example Couldn't find an active session.(401) [{message: "Couldn't find an active session."}]
     # @response Logged out Successfully.(200) [Hash{message: String}]
+    # @response_example Logged out Successfully.(200) [{message: "Logged out Successfully.(200)"}]
     # @tags Users
-    # @no_auth
+    # @auth [bearer_jwt]
     def destroy
       user = current_user
       sign_out(resource_name)
