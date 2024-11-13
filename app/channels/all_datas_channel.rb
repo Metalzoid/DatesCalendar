76f857e991953b1783962097b33625fa719aaf67
Florian GAGNAIRE <gagnaire.flo@gmail.com>
2024-11-13 18:49:09 +0100

    New render allDatasChannel

--- app/channels/all_datas_channel.rb
+++ app/channels/all_datas_channel.rb
@@ -9,21 +9,22 @@ class AllDatasChannel < ApplicationCable::Channel
   end
 
   def self.send_all_datas(current_user)
-    appointments = current_user.appointments.map do |appointment|
-      AppointmentSerializer.new(appointment).serializable_hash[:data][:attributes]
-    end
-    availabilities = current_user.availabilities.map do |availability|
-      AvailabilitySerializer.new(availability).serializable_hash[:data][:attributes]
-    end
-    services = current_user.services.map do |service|
-      ServiceSerializer.new(service).serializable_hash[:data][:attributes]
-    end
+    appointments = current_user.appointments.map { |appointment| AppointmentSerializer.new(appointment).serializable_hash[:data][:attributes] }
+    availabilities = current_user.availabilities.map { |availability| AvailabilitySerializer.new(availability).serializable_hash[:data][:attributes] }
+    services = current_user.services.map { |service| ServiceSerializer.new(service).serializable_hash[:data][:attributes] }
+    customers = current_user.appointments.map(&:customer).uniq(&:id).map { |customer| UserSerializer.new(customer).serializable_hash[:data][:attributes] }
+    sellers = current_user.appointments.map(&:seller).uniq(&:id).map { |seller| UserSerializer.new(seller).serializable_hash[:data][:attributes] }
+
     data = {
       user: UserSerializer.new(current_user).serializable_hash[:data][:attributes],
-      appointments:,
-      availabilities:,
-      services:
-  }
+      appointments: appointments,
+      availabilities: availabilities,
+      services: services
+    }
+
+    data[:customers] = customers if customers.present?
+    data[:sellers] = sellers if sellers.present?
+
     ActionCable.server.broadcast("all_datas_user#{current_user.id}", data)
   end
 end
