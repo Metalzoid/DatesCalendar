58ca9ee489da349f9655af8605a21cf06615f9ac
Florian GAGNAIRE <gagnaire.flo@gmail.com>
2024-12-20 09:41:36 +0100

    fix channel

--- app/channels/new_appointments_channel.rb
+++ app/channels/new_appointments_channel.rb
@@ -8,8 +8,8 @@ class NewAppointmentsChannel < ApplicationCable::Channel
     stop_all_streams
   end
 
-  def self.send_appointments
-    appointments = current_user.appointments
+  def self.send_appointments(user)
+    appointments = user.appointments
     .select { |appointment| appointment.status == "hold" && appointment.start_date > Time.now }
     .map { |appointment| AppointmentSerializer.new(appointment).serialzable_hash.dig(:data, :attributes)}
 
@@ -17,6 +17,6 @@ class NewAppointmentsChannel < ApplicationCable::Channel
       appointments: appointments,
     }
 
-    ActionCable.server.broadcast("new_appointments_#{current_user.id}", data)
+    ActionCable.server.broadcast("new_appointments_#{user.id}", data)
   end
 end
